release 1.0.12 fixing phase number in stefan func and units of selenographic sun coords

src/spicedmoon/spicedmoon.py
@@ -873,6 +873,45 @@ def get_moon_datas_from_moon(lat: float, lon: float, altitude: float,
         correct_zenith_azimuth, lat, lon, False, ignore_bodvrd)
 
 
+def _get_moon_datas_xyzs_no_zenith_azimuth(
+        xyz: Tuple[float, float, float], et: float, source_frame: str, target_frame: str
+    ) -> Tuple[float, float, float, float, float, float, float, float]:
+    sun_pos_moonref, lightime = spice.spkpos('SUN', et, target_frame, 'NONE', 'MOON')
+    sun_pos_satref, lighttime = spice.spkpos('SUN', et, source_frame, 'NONE', 'EARTH')
+    moon_pos_satref, lightime = spice.spkpos('MOON', et, source_frame, 'NONE', 'EARTH')
+    rotation = spice.pxform(source_frame, target_frame, et)
+    # set moon center as zero point
+    sat_pos_translate = np.zeros(3)
+    sat_pos_translate[0] = xyz[0] - moon_pos_satref[0]
+    sat_pos_translate[1] = xyz[1] - moon_pos_satref[1]
+    sat_pos_translate[2] = xyz[2] - moon_pos_satref[2]
+    sat_pos_moonref = spice.mxv(rotation, sat_pos_translate)
+    # selenographic coordinates
+    # sun 
+    sel_lon_sun = np.arctan2(sun_pos_moonref[1], sun_pos_moonref[0])
+    sel_lat_sun = np.arctan2(sun_pos_moonref[2], 
+                        np.sqrt(sun_pos_moonref[0]* sun_pos_moonref[0] + 
+                                sun_pos_moonref[1] * sun_pos_moonref[1]))
+    distance_sun_moon = np.sqrt( sun_pos_moonref[0] * sun_pos_moonref[0] + 
+                                sun_pos_moonref[1] * sun_pos_moonref[1] + 
+                                sun_pos_moonref[2] * sun_pos_moonref[2] )
+    dist_sun_moon_au = spice.convrt(distance_sun_moon, "KM", "AU")
+    # sat
+    sel_lon_sat = np.arctan2(sat_pos_moonref[1], sat_pos_moonref[0])*180.0 / np.pi
+    sel_lat_sat = np.arctan2(sat_pos_moonref[2],
+                        np.sqrt(sat_pos_moonref[0] * sat_pos_moonref[0] +
+                        sat_pos_moonref[1] * sat_pos_moonref[1])) * 180.0 / np.pi
+    distance_sat_moon = np.sqrt(
+                        sat_pos_moonref[0] * sat_pos_moonref[0] + 
+                        sat_pos_moonref[1] * sat_pos_moonref[1] + 
+                        sat_pos_moonref[2] * sat_pos_moonref[2] )
+    # phase
+    phase = (180.0 / np.pi) * np.arccos((sun_pos_moonref[0] * sat_pos_moonref[0] + 
+        sun_pos_moonref[1] * sat_pos_moonref[1] + 
+        sun_pos_moonref[2] * sat_pos_moonref[2]) / 
+        (distance_sat_moon*distance_sun_moon))
+    return dist_sun_moon_au, distance_sun_moon, distance_sat_moon, sel_lon_sun, sel_lat_sun, sel_lat_sat, sel_lon_sat, phase
+
 def get_moon_datas_xyzs_no_zenith_azimuth(
         xyzs: List[Tuple[float, float, float]], dts: List[str], kernels_path: str, source_frame: str = 'J2000', target_frame: str = 'MOON_ME'
     ) -> List[MoonData]:
@@ -905,40 +944,15 @@ def get_moon_datas_xyzs_no_zenith_azimuth(
     mds = []
     for xyz, dt in zip(xyzs, dts):
         et = spice.str2et(dt)
-        sun_pos_moonref, lightime = spice.spkpos('SUN', et, target_frame, 'NONE', 'MOON')
-        sun_pos_satref, lighttime = spice.spkpos('SUN', et, source_frame, 'NONE', 'EARTH')
-        moon_pos_satref, lightime = spice.spkpos('MOON', et, source_frame, 'NONE', 'EARTH')
-        rotation = spice.pxform(source_frame, target_frame, et)
-        # set moon center as zero point
-        sat_pos_translate = np.zeros(3)
-        sat_pos_translate[0] = xyz[0] - moon_pos_satref[0]
-        sat_pos_translate[1] = xyz[1] - moon_pos_satref[1]
-        sat_pos_translate[2] = xyz[2] - moon_pos_satref[2]
-        sat_pos_moonref = spice.mxv(rotation, sat_pos_translate)
-        # selenographic coordinates
-        # sun 
-        sel_lon_sun = np.arctan2(sun_pos_moonref[1], sun_pos_moonref[0]) * 180.0 / np.pi
-        sel_lat_sun = np.arctan2(sun_pos_moonref[2], 
-                            np.sqrt(sun_pos_moonref[0]* sun_pos_moonref[0] + 
-                                    sun_pos_moonref[1] * sun_pos_moonref[1])) * 180.0 / np.pi
-        distance_sun_moon = np.sqrt( sun_pos_moonref[0] * sun_pos_moonref[0] + 
-                                    sun_pos_moonref[1] * sun_pos_moonref[1] + 
-                                    sun_pos_moonref[2] * sun_pos_moonref[2] )
-        dist_sun_moon_au = spice.convrt(distance_sun_moon, "KM", "AU")
-        # sat
-        sel_lon_sat = np.arctan2(sat_pos_moonref[1], sat_pos_moonref[0])*180.0 / np.pi
-        sel_lat_sat = np.arctan2(sat_pos_moonref[2],
-                            np.sqrt(sat_pos_moonref[0] * sat_pos_moonref[0] +
-                            sat_pos_moonref[1] * sat_pos_moonref[1])) * 180.0 / np.pi
-        distance_sat_moon = np.sqrt(
-                            sat_pos_moonref[0] * sat_pos_moonref[0] + 
-                            sat_pos_moonref[1] * sat_pos_moonref[1] + 
-                            sat_pos_moonref[2] * sat_pos_moonref[2] )
-        # phase
-        phase = (180.0 / np.pi) * np.arccos((sun_pos_moonref[0] * sat_pos_moonref[0] + 
-            sun_pos_moonref[1] * sat_pos_moonref[1] + 
-            sun_pos_moonref[2] * sat_pos_moonref[2]) / 
-            (distance_sat_moon*distance_sun_moon))
+        (
+            dist_sun_moon_au, distance_sun_moon, distance_sat_moon,
+            sel_lon_sun, sel_lat_sun, sel_lat_sat, sel_lon_sat, phase
+        ) = _get_moon_datas_xyzs_no_zenith_azimuth(xyz, et, source_frame, target_frame)
+        
+        et_2 = et + 1
+        _, _, _, _, _, _, _, phase2 = _get_moon_datas_xyzs_no_zenith_azimuth(xyz, et_2, source_frame, target_frame)
+        if phase2 < phase:
+            phase = -phase
         md = MoonData(dist_sun_moon_au, distance_sun_moon, distance_sat_moon, sel_lon_sun, sel_lat_sun,
                       sel_lat_sat, sel_lon_sat, phase, None, None)
         mds.append(md)